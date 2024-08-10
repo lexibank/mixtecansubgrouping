fab4cd0b455e706cef73ef410df9d5169742eb87
chrzyki <christoph@functor-argument.net>
2024-08-10 20:35:41 +0200

    Fix metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import json
+
+
+with open("metadata.json", encoding="utf-8") as fp:
+    metadata = json.load(fp)
 
 
 setup(
     name='lexibank_mixtecansubgrouping',
+    description=metadata["title"],
     py_modules=['lexibank_mixtecansubgrouping'],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
     include_package_data=True,
     packages=find_packages(where="."),
     zip_safe=False,
